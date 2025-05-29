8794afa8870fd9692c6a64b4522decff24db9653
SamiraDzhabbarova <samira.dzhabbarova@gmail.com>
2025-05-29 17:08:37 +0300

    Create app.py

---
+++ 3lab/lab3/app/celery/app.py
@@ -0,0 +1,11 @@
+from celery import Celery
+
+celery_app = Celery(
+    "worker",
+    broker="redis://localhost:6379/0",
+    backend="redis://localhost:6379/0"
+)
+
+celery_app.conf.task_track_started = True
+celery_app.conf.task_serializer = "json"
+celery_app.conf.result_serializer = "json"
